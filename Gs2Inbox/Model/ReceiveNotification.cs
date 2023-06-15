8323858a08e9b41ddb7c395a18c4413ce9311447
gs2-ci <ci@gs2.io>
2023-06-15 20:26:50 +0000

    f6f294b91bb531f2f72d6df453d7a55ba6711f1e

--- Gs2Inbox/Model/ReceiveNotification.cs
+++ Gs2Inbox/Model/ReceiveNotification.cs
@@ -21,9 +21,14 @@ namespace Gs2.Gs2Inbox.Model
 	public class ReceiveNotification
 	{
         public string NamespaceName { set; get; }
+        public string UserId { set; get; }
         public string MessageName { set; get; }
         public ReceiveNotification WithNamespaceName(string namespaceName) {
             this.NamespaceName = namespaceName;
+            return this;
+        }
+        public ReceiveNotification WithUserId(string userId) {
+            this.UserId = userId;
             return this;
         }
         public ReceiveNotification WithMessageName(string messageName) {
@@ -41,6 +46,7 @@ namespace Gs2.Gs2Inbox.Model
             }
             return new ReceiveNotification()
                 .WithNamespaceName(!data.Keys.Contains("namespaceName") || data["namespaceName"] == null ? null : data["namespaceName"].ToString())
+                .WithUserId(!data.Keys.Contains("userId") || data["userId"] == null ? null : data["userId"].ToString())
                 .WithMessageName(!data.Keys.Contains("messageName") || data["messageName"] == null ? null : data["messageName"].ToString());
         }
     }
