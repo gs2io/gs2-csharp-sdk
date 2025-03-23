56846900fbf3510802cfb6963f2fd276a02e3240
gs2-ci <ci@gs2.io>
2025-03-23 10:33:21 +0000

    3c06996a2decc071453f423ad6ea6d3123b4c527

--- Gs2Chat/Domain/Gs2Chat.cs
+++ Gs2Chat/Domain/Gs2Chat.cs
@@ -12,6 +12,8 @@
  * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
+ *
+ * deny overwrite
  */
 // ReSharper disable RedundantNameQualifier
 // ReSharper disable RedundantUsingDirective
@@ -573,7 +575,7 @@ namespace Gs2.Gs2Chat.Domain
                 string payload
         ) {
             switch (action) {
-                case "PostNotification": {
+                case "Post": {
                     var notification = PostNotification.FromJson(JsonMapper.ToObject(payload));
                     _gs2.Cache.RequireListCacheUpdate<Gs2.Gs2Chat.Model.Message>(
                         (null as Gs2.Gs2Chat.Model.Message).CacheParentKey(
